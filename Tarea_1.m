c3585deac14e115f9545b076db96b6da7580fa4f
FosterTooth1 <albertoflores91396@gmail.com>
2024-03-04 18:46:05 -0600

    Tarea corregida

--- Tarea_1.m
+++ Tarea_1.m
@@ -30,12 +30,14 @@ Precision=input('Ingrese el numero de precision del algoritmo: ');
 
 %Calculamos la cantidad de bits para cada variable 
 for i=1:Num_var 
-    Num_bits(i)=fix(log2(Limite_Superior(i)-Limite_Inferior(i)) * (10 ^ Precision) + 0.9); 
+    Num_bits(i)=fix(log2((Limite_Superior(i)-Limite_Inferior(i)) * 10 ^ Precision) + 0.9); 
 end
 
 %Generamos la población en una matriz m*n donde m es la poblacion y n son
 %la suma de los bits de todas las variables
 Poblacion = randi([0 1],[Num_pob sum(Num_bits)]); 
+
+disp(Poblacion)
 
 % Convertir la población binaria a población real
 Poblacion_real = zeros(Num_pob, Num_var);
@@ -48,26 +50,50 @@ for i = 1:Num_pob
         bit_final = bit_inicio + Num_bits(j) - 1;
         binario = Poblacion(i, bit_inicio:bit_final);
         %Lo convertimos de binario a decimal
-        %disp('Binario')
-        %disp(binario)
+        disp('Binario')
+        disp(binario)
         valor_binario = binario_a_decimal(binario);
-        %disp('Decimal')
-        %disp(valor_binario)
+        disp('Decimal')
+        disp(valor_binario)
         %Hacemos la conversion a reales
         Poblacion_real(i, j) = Limite_Inferior(j) + ((valor_binario * (Limite_Superior(j) - Limite_Inferior(j) )) / ((2 ^ Num_bits(j))-1));
-        %disp("Valor real")
-        %disp(Poblacion_real(i,j))
+        disp("Valor real")
+        disp(Poblacion_real(i,j))
         bit_inicio = bit_final + 1;
     end
 end
 
 % Evaluación de la población en la función objetivo
 for i = 1:Num_pob
-    funcion_eva(i) = (1-Poblacion_real(i,1))^2 + (100-Poblacion_real(i,2))^2;
+    aptitud(i) = (1-Poblacion_real(i,1))^2 + (100-Poblacion_real(i,2))^2;
 end
 
-disp(funcion_eva)
+for i = 1:Num_pob
+    disp(['Esta es la aptitud del individuo ', num2str(i), ': ']);
+    disp(aptitud(i));
+end
 
+
+%Codigo del Torneo
+Padres = zeros(Num_pob,sum(Num_bits));
+Torneo = [randperm(Num_pob); randperm(Num_pob)]';
+
+for i = 1:Num_pob
+    
+    if aptitud(Torneo(i,1))<aptitud(Torneo(i,2))
+        Padres(i,:) = Poblacion(Torneo(i,1),:);
+    
+    else
+        Padres(i,:) = Poblacion(Torneo(i,2),:);
+      
+    end
+end
+
+
+for i = 1:Num_pob
+    disp(['Esta es la codificacion en bits del Padre ', num2str(i), ':']);
+    disp(Padres(i,:));
+end
 
 function decimal = binario_a_decimal(binario)
     % Inicializa el valor decimal
